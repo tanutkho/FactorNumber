b3910aae26dd24fbacfe63040a0abc24eab4a2b5
Kubo <t_nutji@hotmail.com>
2018-06-03 10:09:22 +0700

    Near OK

--- FactorNumber/ViewController.swift
+++ FactorNumber/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var factorTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func resultButton(_ sender: Any) {
+        
+        let numberString = factorTextField.text
+        print("numberString ==> \(String(describing: numberString))")
+        
+        var timesInt = 1
+        var showResultStrings = [""]
+        var showResultString = ""
+        
+        
+        while timesInt <= 12 {
+            
+            let answerInt = Int(numberString!)! * timesInt
+            showResultStrings.append(String(answerInt))
+            
+            
+            showResultString = showResultString + showResultStrings[timesInt] + "\n"
+            
+            print("Answer ==> \(showResultString)")
+            
+            timesInt += 1
+        }
+        
+        resultLabel.text = showResultString
+        
+    } //resultButton
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
